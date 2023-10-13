f07bfe137fe6b15e998945d968538418441844dd
Felipe <felipe.vilela01@outlook.com>
2023-10-13 10:24:53 -0300

    Criação dos submenus

--- sistemaHospital.py
+++ sistemaHospital.py
@@ -10,7 +10,49 @@ def menu():
 
 	return opc
 
+def subMenu(opcao, medicos, pacientes, consultas):
+    print("SUBMENU DE OPÇÕES")
+
+    if opcao == 1:
+        print("1. Incluir Médico")
+        print("2. Alterar Médico")
+        print("3. Excluir Médico")
+        print("4. Listar Médico")
+        print("5. Listar Todos")
+        print("6. Sair")
+        opc = int(input("Qual opção você deseja? "))
+   
+    elif opcao == 2:
+        print("1. Incluir Paciente")
+        print("2. Alterar Paciente")
+        print("3. Excluir Paciente")
+        print("4. Listar Paciente")
+        print("5. Listar Todos")
+        print("6. Sair")
+        opc = int(input("Qual opção você deseja? "))
+
+    elif opcao == 3:
+        print("1. Incluir Consulta")
+        print("2. Alterar Consulta")
+        print("3. Excluir Consulta")
+        print("4. Listar Consulta")
+        print("5. Listar Todas")
+        print("6. Sair")
+        opc = int(input("Qual opção você deseja? "))
+
+    elif opcao == 4:
+        print("1. Mostrar medicos com a especialização X")
+        print("2. Mostrar pacientes menores de X idade")
+        print("3. Mostrar o CRM, o nome do médico, o CPF do paciente, o nome do paciente, data, hora, diagnostico e medicamentos para todas as consultas realizadasnos últimos X dias")
+        opc = int(input("Qual opção você deseja? "))   
+        
+    return opc
+
 def main():
-	menu()
+    medicos = dict()
+    pacientes = dict()
+    consultas = dict()
+
+    subMenu(menu(), medicos, pacientes, consultas)
 
 main()
