012d8feb79d52bca424eaf9395f3401531c43367
Felipe <felipe.vilela01@outlook.com>
2023-10-21 10:29:05 -0300

    Criação inserir paciente

--- sistemaHospital.py
+++ sistemaHospital.py
@@ -10,12 +10,13 @@ def menu():
 
 	return opc
 
+# DICIONÁRIO DOS MÉDICOS
 def inserirMedicos(medicos, emails, telefones):
     crm = str(input("Digite o CRM: ")).lower()
     nome = str(input("Digite o seu nome: ")).lower()
-    data_nascimento = str(input("Digite a dat de nascimento (xx/xx/xxxx): ")).lower()
-    sexo = str(input("Digite o sexo (M/F) : ")).lower()
-    especialidade = str(input("Digite a especialidade: ")).lower()
+    data_nascimento = str(input("Digite a data de nascimento (xx/xx/xxxx): ")).lower()
+    sexo = str(input("Digite o seu sexo (M/F) : ")).lower()
+    especialidade = str(input("Digite a sua especialidade: ")).lower()
     universidade = str(input("Digite a universidade em que se formou: ")).lower()
 
     emails = inserirEmail(emails)
@@ -25,6 +26,30 @@ def inserirMedicos(medicos, emails, telefones):
 
     return medicos
 
+# DICIONÁRIO DOS PACIENTES
+# Paciente = (CPF, Nome, Data de Nascimento, Sexo, Plano de Saúde, E-mails, Telefones)
+def inserirPacientes(pacientes, emails, telefones):
+    cpf = str(input("Digite o CPF: ")).lower()
+    nome = str(input("Digite o seu nome: ")).lower()
+    data_nascimento = str(input("Digite a data de nascimento (xx/xx/xxxx): ")).lower()
+    sexo = str(input("Digite o seu sexo (M/F) : ")).lower()
+    plano_saude = str(input("Digite o seu plano de saúde: ")).lower()
+    
+    emails = inserirEmail(emails)
+    telefones = inserirTelefones(telefones)
+
+    pacientes[cpf] = [nome, data_nascimento, sexo, plano_saude, emails, telefones]
+
+    return pacientes
+
+
+# DICIONÁRIO DA CONSULTA
+# Consulta = (CRM, CPF, Data, Hora, Diagnostico, Medicamentos)
+def inserirConsultas(consultas):
+
+    return consultas
+
+# SUBFUNÇÕES
 def inserirEmail(emails):
     email = str(input("Digite o e-mail: ")).lower()
     
@@ -44,7 +69,7 @@ def inserirTelefones(telefones):
     
     return telefones
 
- 
+# FUNÇÕES PRINCIPAIS
 def subMenu(opcao, medicos, pacientes, consultas):
     while opcao <= 4:
         emails = []
@@ -60,7 +85,7 @@ def subMenu(opcao, medicos, pacientes, consultas):
             opc = int(input("Qual opção você deseja? "))
 
             if opc == 1:
-                print(inserirMedicos(medicos, emails, telefones))
+                inserirMedicos(medicos, emails, telefones)
 
         elif opcao == 2:
             print("1. Incluir Paciente")
@@ -70,6 +95,9 @@ def subMenu(opcao, medicos, pacientes, consultas):
             print("5. Listar Todos")
             opc = int(input("Qual opção você deseja? "))
 
+            if opc == 1:
+                inserirPacientes(pacientes, emails, telefones)
+
         elif opcao == 3:
             print("1. Incluir Consulta")
             print("2. Alterar Consulta")
@@ -77,6 +105,9 @@ def subMenu(opcao, medicos, pacientes, consultas):
             print("4. Listar Consulta")
             print("5. Listar Todas")
             opc = int(input("Qual opção você deseja? "))
+
+            if opc == 1:
+                inserirConsultas(consultas)
 
         elif opcao == 4:
             print("1. Mostrar medicos com a especialização X")
@@ -95,9 +126,10 @@ def main():
     consultas = dict()
 
     subMenu(menu(), medicos, pacientes, consultas)
+
+    print(medicos)
+    print(pacientes)
+    print(consultas)
     
     
 main()
-
-
-
